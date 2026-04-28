9a68212cd2025a11f07d4b797de5f71cf3cc7013
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-28 09:51:44 -0300

    chore: add react router dom setup and react query

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Authentication from "./pages/authentication";
+
+const queryClient = new QueryClient();
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Authentication />} index />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Authentication />} index />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
