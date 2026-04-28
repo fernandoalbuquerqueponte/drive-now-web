80ceaa34c28aa8370648e6b1bd83f692c8564365
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-28 17:01:34 -0300

    refactor: replace window.location with useNavigate for better routing in Authentication component

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import LoginForm from "@/components/login-form";
 import SignUpForm from "@/components/sign-up-form";
@@ -7,18 +8,20 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import icon from "../assets/icon-removebg-preview.png";
 
 function Authentication() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
     if (token) {
-      window.location.href = "/home";
+      navigate("/home");
     }
-  });
+  }, [navigate]);
 
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-3 px-6">
       <img src={icon} alt="icone" className="h-65 w-auto object-contain" />
 
-      <Tabs defaultValue="login" className="w-100">
+      <Tabs defaultValue="login" className="w-full max-w-100">
         <TabsList className="grid w-full grid-cols-2" variant="line">
           <TabsTrigger value="create-account">Criar Conta</TabsTrigger>
           <TabsTrigger value="login">Login</TabsTrigger>
