163e28169e94b5db9bf52e2cb379ea7173130b39
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-05-13 11:08:21 -0300

    feat: add UseGetUserResponse type for user data fetching and update useGetUser hook to utilize it

---
+++ src/http/types/use-edit-user-profile-response.ts
@@ -0,0 +1,7 @@
+export type UseGetUserResponse = {
+  first_name?: string;
+  last_name?: string;
+  email?: string;
+  password?: string;
+  imageUrl?: string;
+};
--- src/http/use-get-user.ts
+++ src/http/use-get-user.ts
@@ -1,12 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { api } from "./api-client";
+import type { UseGetUserResponse } from "./types/use-edit-user-profile-response";
 
 export function useGetUser() {
   return useQuery({
     queryKey: ["get-user"],
     queryFn: async () => {
-      const response = await api.get(`/api/users`);
+      const response = await api.get<UseGetUserResponse>(`/api/users`);
 
       return response.data;
     },
