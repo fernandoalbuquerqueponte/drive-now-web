075b317fce7fa7cd1b3ad54363cd5adc3607a0a9
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-05-19 13:11:22 -0300

    feat: add animated dot pattern and aurora text components; update home page layout and styles

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import { Clock, Medal, Shield } from "lucide-react";
 import { parseAsString, useQueryStates } from "nuqs";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -5,8 +6,12 @@ import { useNavigate } from "react-router-dom";
 import CarCard from "@/components/car-card";
 import FilterInput from "@/components/filter-input";
 import Header from "@/components/header";
+import { AuroraText } from "@/components/ui/aurora-text";
+import { Card, CardContent } from "@/components/ui/card";
+import { DotPattern } from "@/components/ui/dot-pattern";
 import { Spinner } from "@/components/ui/spinner";
 import { useCars } from "@/http/use-cars";
+import { cn } from "@/lib/utils";
 
 export interface CarSpecification {
   label: string;
@@ -57,25 +62,76 @@ function HomePage() {
     setFilters({
       [key]: value === "all" || value === "" ? null : value,
     });
-
-    console.log(value);
   };
+
   return (
     <div className="w-full">
       <Header />
 
-      <div className="container mx-auto w-full px-5 py-20">
-        <div className="mb-16 flex w-full flex-col items-center gap-3 px-4 text-center">
-          {" "}
-          <h1 className="text-3xl leading-tight font-bold md:text-5xl">
+      <div className="relative flex min-h-125 flex-col items-center justify-center space-y-12 md:h-145">
+        <DotPattern
+          className={cn(
+            "mask-[radial-gradient(ellipse_at_center,white,transparent)]",
+          )}
+        />
+        <div className="relative z-10 container mx-auto flex flex-col items-center gap-3 px-5 text-center">
+          <AuroraText
+            colors={["#FFFFFF", "#E4E4E7", "#9A9A9A"]}
+            className="text-3xl leading-tight font-bold md:text-5xl"
+          >
             Alugue o Carro dos Seus Sonhos
-          </h1>
+          </AuroraText>
           <p className="text-muted-foreground max-w-2xl text-lg text-pretty md:text-xl">
             Descubra nossa frota premium de veículos disponíveis para aluguel
             por hora. Qualidade, conforto e segurança garantidos.
           </p>
         </div>
+        <div className="relative z-10 mt-8 grid w-full max-w-5xl grid-cols-1 gap-6 px-5 md:grid-cols-3">
+          <Card className="w-full">
+            <CardContent className="flex flex-col items-center space-y-3 text-center">
+              <div className="flex flex-col items-center gap-2">
+                <div className="rounded-full bg-zinc-700 p-3 text-center">
+                  <Shield />
+                </div>
+                <h2 className="text-lg font-bold">Seguro total</h2>
+              </div>
+              <p className="text-muted-foreground text-sm">
+                Proteção completa em todos os veículos
+              </p>
+            </CardContent>
+          </Card>
 
+          <Card className="w-full">
+            <CardContent className="flex flex-col items-center space-y-3 text-center">
+              <div className="flex flex-col items-center gap-2">
+                <div className="rounded-full bg-zinc-700 p-3 text-center">
+                  <Clock />
+                </div>
+                <h2 className="text-lg font-bold">Reserva Rápida</h2>
+              </div>
+              <p className="text-muted-foreground text-sm">
+                Processo simples em poucos cliques
+              </p>
+            </CardContent>
+          </Card>
+
+          <Card className="w-full">
+            <CardContent className="flex flex-col items-center space-y-3 text-center">
+              <div className="flex flex-col items-center gap-2">
+                <div className="rounded-full bg-zinc-700 p-3 text-center">
+                  <Medal />
+                </div>
+                <h2 className="text-lg font-bold">Qualidade Garantida</h2>
+              </div>
+              <p className="text-muted-foreground text-sm">
+                Veículos premium, sempre revisados
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+
+      <div className="container mx-auto w-full px-5 py-20">
         <FilterInput filters={filters} onFilterChange={handleFilterChange} />
 
         {!isLoading && (
@@ -91,7 +147,7 @@ function HomePage() {
               <Spinner className="size-7" />
             </div>
           ) : (
-            data?.map((item: Car) => <CarCard item={item} />)
+            data?.map((item: Car) => <CarCard key={item.id} item={item} />)
           )}
         </div>
       </div>
