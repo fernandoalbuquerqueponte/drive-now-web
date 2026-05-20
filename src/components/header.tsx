b6cc94fec944457ebdc994714b3e7bf910c50b27
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-05-20 15:43:23 -0300

    feat: enhance header component with user data and improve layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,76 +12,71 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useGetUser } from "@/http/use-get-user";
 
 function Header() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
+  const { data: user } = useGetUser();
+
   const handleLogout = () => {
-    // 1. Limpa os tokens
     localStorage.removeItem("accessToken");
     localStorage.removeItem("refreshToken");
-
-    // 2. Limpa o cache do React Query
-    // (Isso evita que o próximo usuário veja os dados do antigo por um segundo)
     queryClient.clear();
-
-    // 3. Manda para a tela de login ou home
     navigate("/authentication");
   };
+
   return (
-    <div className="flex w-full items-center justify-around border border-zinc-900 bg-transparent py-5">
+    <header className="flex w-full items-center justify-around border border-zinc-900 bg-transparent py-5">
       <Link to="/home">
         <h2 className="text-xl font-bold">DriveNOW</h2>
       </Link>
 
-      <nav className="text-muted-foreground flex items-center gap-6">
-        <a href="/home">Veículos</a>
-        <a href="#">Sobre</a>
-        <a href="#">Contato</a>
-      </nav>
-
-      <div>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon" className="rounded-full">
-              <Avatar>
-                <AvatarImage
-                  src="https://github.com/fernandoalbuquerqueponte.png"
-                  alt="shadcn"
-                />
-                <AvatarFallback>FA</AvatarFallback>
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="w-56">
-            <DropdownMenuGroup>
-              <DropdownMenuItem asChild>
-                <Link to="/account">
-                  <User />
-                  Conta
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon" className="rounded-full">
+            <Avatar size="lg">
+              <AvatarImage
+                src={user?.imageUrl as string}
+                alt={user?.first_name + " " + user?.last_name}
+              />
+              <AvatarFallback>
+                {(user?.first_name[0] as string) +
+                  (user?.last_name[0] as string)}
+              </AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuGroup>
+            <DropdownMenuItem asChild>
+              <Link to="/account">
+                <User />
+                Conta
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link to="/my-cars">
                 <Car />
                 Meus Carros
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link to="/bookings">
-                  <Calendar />
-                  Minhas reservas
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={handleLogout}>
-              <LogOutIcon />
-              Sign Out
+              </Link>
             </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    </div>
+            <DropdownMenuItem asChild>
+              <Link to="/bookings">
+                <Calendar />
+                Minhas reservas
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={handleLogout}>
+            <LogOutIcon />
+            <span className="text-destructive">Sair da conta</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </header>
   );
 }
 
